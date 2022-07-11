69ce8a86ff94150c5511532698b28983ff7b1f41
Renan Cesar <cesar.renan@hotmail.com>
2022-07-11 16:46:00 -0300

    finalizando lógica dos botões

--- JS/main.js
+++ JS/main.js
@@ -3,9 +3,17 @@ const corrente = document.querySelector("#corrente");
 const buttons = document.querySelectorAll("#botoes-container button");
 
 
+class calculadora {
+
+}
+
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const value = e.target.innerText;
-    console.log(value);
+    if(+value >= 0 || value === "."){
+     console.log(value); 
+    } else {
+      console.log(`Operação: ${value}`);
+    };
   })
 });

e0e13783207e49538fa4a2b4beb0b1c02c8d6bf8
Renan Cesar <cesar.renan@hotmail.com>
2022-07-11 16:33:18 -0300

    Adicionando javascript

--- JS/main.js
+++ JS/main.js
@@ -1,96 +1,10 @@
-const ce = document.querySelector("#ce");
-const c = document.querySelector("#c");
-const del = document.querySelector("#del");
-const multiplicar = document.querySelector("#multiplicar");
-const dividir = document.querySelector("#dividir");
-const mais = document.querySelector("#mais");
-const menos = document.querySelector("#menos");
-const igual = document.querySelector("#igual");
-const ponto = document.querySelector("#ponto");
-const zero = document.querySelector("#zero");
-const um = document.querySelector("#um");
-const dois = document.querySelector("#dois");
-const tres = document.querySelector("#tres");
-const quatro = document.querySelector("#quatro");
-const cinco = document.querySelector("#cinco");
-const seis = document.querySelector("#seis");
-const sete = document.querySelector("#sete");
-const oito = document.querySelector("#oito");
-const nove = document.querySelector("#nove");
+const previa = document.querySelector("#previa");
+const corrente = document.querySelector("#corrente");
+const buttons = document.querySelectorAll("#botoes-container button");
 
 
-ce.addEventListener("click",  ()=> {
-  console.log("ce");
-});
-
-c.addEventListener("click", ()=>{
-  console.log("c");
-});
-
-del.addEventListener("click", ()=>{
-  console.log("del");
-});
-
-multiplicar.addEventListener("click", ()=>{
-  console.log("multiplicar");
-});
-
-dividir.addEventListener("click", ()=>{
-  console.log("dividir");
-});
-
-mais.addEventListener("click", ()=>{
-  console.log("mais");
-});
-
-menos.addEventListener("click", ()=>{
-  console.log("menos");
-});
-
-igual.addEventListener("click", ()=> {
-  console.log("Igual");
-});
-
-ponto.addEventListener("click", ()=>{
-  console.log("ponto");
-});
-
-zero.addEventListener("click", ()=>{
-  console.log("zero");
-});
-
-um.addEventListener("click", ()=>{
-  console.log("um");
-});
-
-dois.addEventListener("click", ()=>{
-  console.log("dois");
-});
-
-tres.addEventListener("click", ()=>{
-  console.log("tres");
-});
-
-quatro.addEventListener("click", ()=>{
-  console.log("quatro");
-});
-
-cinco.addEventListener("click", ()=>{
-  console.log("cinco");
-});
-
-seis.addEventListener("click", ()=>{
-  console.log("seis");
-});
-
-sete.addEventListener("click", ()=>{
-  console.log("sete");
-});
-
-oito.addEventListener("click", ()=>{
-  console.log("oito");
-});
-
-nove.addEventListener("click", ()=>{
-  console.log("nove");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log('Clicou');
+  })
 });
